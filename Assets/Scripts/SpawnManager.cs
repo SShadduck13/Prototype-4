ae93297260e57794fac9a217ecb677f4a904eab3
Sadie Shadduck <56174574+SShadduck13@users.noreply.github.com>
2020-02-26 08:51:30 -0800

    Enemy Randomly Spawns

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPoX = Random.Range(-spawnRange, spawnRange);
+        float spawnPoZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPoX, 0, spawnPoZ);
+
+        return randomPos;
     }
 }
